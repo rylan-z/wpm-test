679674faeb1d278e200395e37bb3190c3a598056
Rylan <rylan@rhinodesign.net>
2021-07-31 16:12:06 +1000

    1

--- main.js
+++ main.js
@@ -1,41 +1,66 @@
-var count = document.getElementById('count');
-var input = document.getElementById('input');
-var globalWordCounter = 0;
-var WORD_LIMIT = 10;
+// Use stopwatch as a constructor with `this` being the created object:
+var watch = new Stopwatch();
 
-input.addEventListener('keydown', function(e) {
-  if (globalWordCounter > WORD_LIMIT && e.code !== "Backspace") {
-    e.preventDefault();
-    return alert("You have reached the word limit");
+// Bind the event handlers to both the buttons. No more HTML `onclick` attributes.
+jQuery('#start').click(watch.start);
+jQuery('#stop').click(function(){
+  if (watch.getSeconds() === null) {
+    alert('you did not start yet!');
+    return;
   }
+  //Stop the clock
+  watch.stop();
+  // create wpm and difference here:
+  var wpm = Math.round(wordCount('#page1') / (watch.getSeconds() / 60));
+  var difference = Math.round(100*((wpm/250)-1));
+  // refer to watch's methods, and use text()
+  jQuery('#timeValue').text(watch.getSeconds());
+  // Make sure to replace the previous result, and give some clarity in the output
+  jQuery('#wordValue').text(Math.round(wordCount('#page1')));
+  jQuery('#speed').text(wpm);
+  jQuery('#difference').text(difference + '%');
 });
 
-input.addEventListener('keyup', function(e) {
-  wordCounter(e.target.value);
-});
+function Stopwatch(){
+  var startTime, endTime, instance = this;
 
-function isWord(str) {
-  var alphaNumericFound = false;
-  for (var i = 0; i < str.length; i++) {
-    var code = str.charCodeAt(i);
-    if ((code > 47 && code < 58) || // numeric (0-9)
-        (code > 64 && code < 91) || // upper alpha (A-Z)
-        (code > 96 && code < 123)) { // lower alpha (a-z)
-      alphaNumericFound = true;
-      return alphaNumericFound;
+  this.start = function (){
+    startTime = new Date();
+  };
+
+  this.stop = function (){
+    endTime = new Date();
+  }
+
+  this.clear = function (){
+    startTime = null;
+    endTime = null;
+  }
+
+  this.getSeconds = function(){
+    // return non-numerical value to indicate timer was not started
+    if (!startTime){
+      return null;
     }
+    if (!endTime){
+      return 0;
+    }
+    return Math.round((endTime.getTime() - startTime.getTime()) / 1000);
   }
-  return alphaNumericFound;
+
+  this.getMinutes = function(){
+    return instance.getSeconds() / 60;
+  }      
+  this.getHours = function(){
+    return instance.getSeconds() / 60 / 60;
+  }    
+  this.getDays = function(){
+    return instance.getHours() / 24;
+  }   
 }
 
-function wordCounter(text) {
-  var text = input.value.split(' ');
-  var wordCount = 0;
-  for (var i = 0; i < text.length; i++) {
-    if (!text[i] == ' ' && isWord(text[i])) {
-      wordCount++;
-    }
-  }
-  globalWordCounter = wordCount;
-  count.innerText = wordCount;
+// create separate function that has little to do with the stopwatch:
+function wordCount(text){
+  testWords = (jQuery(text).text().length) / 5;
+  return testWords;
 }
